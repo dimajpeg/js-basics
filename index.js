8ec5d0a0429b42e6e4fc9856529ae74a40b51a2b
dima <>
2025-05-21 23:20:49 +0300

    Update live server port, add basic HTML structure, and initialize JavaScript variables

---
+++ index.js
@@ -0,0 +1,18 @@
+let name = "Dima"
+let lastName = "Matuschenko"
+let fullName = name + " " + lastName
+const age = 20
+// let 7isMarried = false
+// let .adress = "Ukraine"
+// let let = "Dima"
+// let is married = false
+// const =7
+console.log(fullName, age)
+/*
+ну вот пишу тут обьясняю
+как работает мой код 
+*/
+
+let filmActor = "Vinston" // camelCase
+let FullTime = 24 //PascalCase
+let full_name = "frank-richard" //snake_case
