3085f5dab8adf31b82e1a1b33bb6278669c337cf
dima <>
2025-06-17 14:46:12 +0300

    Add welcome message, age check form, and event listeners for user interaction

--- index.js
+++ index.js
@@ -16,3 +16,31 @@ console.log(fullName, age)
 let filmActor = "Vinston" // camelCase
 let FullTime = 24 //PascalCase
 let full_name = "frank-richard" //snake_case
+
+const myButton = document.getElementById("my-button")
+const yearInput = document.getElementById("year-input")
+const ageForm = document.getElementById("age-form")
+const checkAgeButton = document.getElementById("check-age-button")
+
+myButton.addEventListener('click', function() {
+  let userName = prompt("Будь ласка, введіть своє ім'я:")
+  console.log("Привіт, " + userName + "!")
+})
+
+ageForm.addEventListener('submit', function(event) {
+  event.preventDefault()
+  const userAge = 2025 - yearInput.value
+  console.log("Вам " + userAge + " років")
+})
+
+
+checkAgeButton.addEventListener('click', function() {
+  let userAge = ageInput.value
+  if (userAge >= 18) {
+    console.log("Ви є повнолітним.")
+  } else {
+    console.log("Ви є неповнолітним.")
+  }
+})
+
+
